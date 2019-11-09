d8b1abcee581fad7f791fef02fe8627ce0e7cd89
SubhoBasak <subhobasak50@gmail.com>
2019-11-09 09:18:36 +0530

    add python webbrowser little tutorial programme

---
+++ python_webbrowser.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+brower = webbrowser.get('c:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s')
+
+brower.open('google.com')
+brower.open_new_tab('stackoverflow.com')
+
+brower.open_new('web.whatsapp.com')
